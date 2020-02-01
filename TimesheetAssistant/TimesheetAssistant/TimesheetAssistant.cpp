994af050477b7a55b086c81e1caac6ff3c725cd8
Alexis Morel <alexismoreldev@gmail.com>
2020-02-01 13:25:23 -0500

    delete visual studio things

--- TimesheetAssistant/TimesheetAssistant/TimesheetAssistant.cpp
+++
@@ -1,8 +0,0 @@
-#include "TimesheetAssistant.h"
-
-#include <ui_TimesheetAssistant.h>
-
-TimesheetAssistant::TimesheetAssistant(QWidget *parent)
-    : QMainWindow(parent), m_pUi(std::make_shared<Ui::TimesheetAssistant>()) {
-  m_pUi->setupUi(this);
-}
