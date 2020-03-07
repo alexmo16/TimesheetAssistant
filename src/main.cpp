f8bb7ec6a10be7d0009e779f0c5069e5ce192ef6
Alexis Morel <alexismoreldev@gmail.com>
2020-03-07 15:16:56 -0500

    add refresh button

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
 #include "Model/ModelThread.h"
 #include "View/MainWindow.h"
 
+#include <QDebug>
+#include <QResource>
 #include <QtWidgets/QApplication>
 #pragma comment( lib, "wevtapi.lib" )
 
 int main( int argc, char* argv[] )
 {
+	QResource::registerResource( "./GeneratedFiles/TimesheetAssistant.rcc" );
 	QApplication a( argc, argv );
 
 	std::shared_ptr<Model::ModelThread> modelThread = std::make_shared<Model::ModelThread>();
